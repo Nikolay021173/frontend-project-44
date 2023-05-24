10cfb48a8933c0e337dd31e992b29b19637336aa
Nikolay021173 <nikolay021173@gmail.com>
2023-05-25 00:34:04 +0300

    recent changes

---
+++ bin/brain-calc.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import * as readlineSync from 'readline-sync';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const dialogWithTheUser = () => {
+  console.log('brain-even');
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('What is the result of the expression?');
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = getRandomInt(0, 100);
+    const num2 = getRandomInt(0, 100);
+    const operators = ['+', '-', '*', '/'];
+    const op = operators[Math.floor(Math.random() * operators.length)];
+    const result = eval(`${num1}${op}${num2}`);
+    console.log(`Question: ${num1}${op}${num2}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (result === Number(answer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+dialogWithTheUser();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,37 +1,36 @@
 #!/usr/bin/env node
+
 import * as readlineSync from 'readline-sync';
 
-const getRandomInt = (min, max) => {
-return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const dialogWithTheUser = () => {
   console.log('brain-even');
- console.log('Welcome to the Brain Games!');
- const name = readlineSync.question('May I have your name? ');
- console.log('Hello, ' + name + '!');
- console.log('Answer "yes" if the number is even, otherwise answer "no".');
-for (let i = 0; i < 3; i += 1) {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
     const num = getRandomInt(0, 100);
-    console.log('Question: ' + num);
+    console.log(`Question: ${num}`);
     const answer = readlineSync.question('Your answer: ');
-    if((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer === 'no')) {
-        console.log('Correct!'); 
+    if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer === 'no')) {
+      console.log('Correct!');
     } else if ((num % 2 === 0 && answer === 'no')) {
-        console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.`);
-        console.log("Let's try again, " + name + '!');
-        return;
+      console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
+      console.log(`Let's try again, ${name}!`);
+      return;
     } else if ((num % 2 !== 0 && answer === 'yes')) {
-        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.`);
-        console.log("Let's try again, " + name + '!');
-        return;
-} else {
-    console.log("input error!!!!\nAs an answer, you have to enter 'yes' or 'no'.");
-    console.log("Let's try again, " + name + '!');
-    return;
-}
-}
-console.log('Congratulations, ' + name + '!');
+      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+      console.log(`Let's try again, ${name}!`);
+      return;
+    } else {
+      console.log("input error!!!!\nAs an answer, you have to enter 'yes' or 'no'.");
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
 dialogWithTheUser();
